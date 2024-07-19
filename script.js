7575f0f56ba48bf8edd4f32320a06a51e5029281
Your Name <you@example.com>
2024-07-19 13:56:49 +0200

    #2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const button = document.querySelector('.card-btn')
-const blogCard = document.querySelector('.blog-card')
+const button = document.querySelector(".card-btn");
+const blogCard = document.querySelector(".blog-card");
 
-button.addEventListener('click', ()=>{
-    blogCard.classList.toggle("change")?button.firstElementChild.textContent=" Moins": button.firstElementChild.textContent=" Plus"
-})
+button.addEventListener("click", () => {
+  blogCard.classList.toggle("change")
+    ? (button.firstElementChild.textContent = "Moins")
+    : (button.firstElementChild.textContent = "Plus");
+});
